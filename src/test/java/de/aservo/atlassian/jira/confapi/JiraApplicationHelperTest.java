c42431f953fece6e655812e74ac7a9f32cb35628
Patrick Hobusch <patrick.hobusch@gmail.com>
2018-08-13 12:51:51 +0200

    Add tests for JiraApplicationHelper

---
+++ src/test/java/de/aservo/atlassian/jira/confapi/JiraApplicationHelperTest.java
@@ -0,0 +1,78 @@
+package de.aservo.atlassian.jira.confapi;
+
+import com.atlassian.jira.application.ApplicationKeys;
+import com.atlassian.jira.bc.license.JiraLicenseService;
+import com.atlassian.jira.license.JiraLicenseManager;
+import com.atlassian.jira.license.LicenseDetails;
+import com.atlassian.jira.license.LicensedApplications;
+import com.atlassian.jira.license.MockLicensedApplications;
+import com.atlassian.jira.util.I18nHelper;
+import com.atlassian.jira.util.SimpleErrorCollection;
+import com.atlassian.jira.web.bean.MockI18nBean;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class JiraApplicationHelperTest {
+
+    private static final String LICENSE = "AAA...";
+
+    @Mock
+    private JiraLicenseManager licenseManager;
+
+    @Mock
+    private JiraLicenseService licenseService;
+
+    private LicenseDetails mockLicenseDetail;
+
+    private JiraApplicationHelper applicationHelper;
+
+    @Before
+    public void setup() {
+        final LicensedApplications licensedApplications = new MockLicensedApplications(ApplicationKeys.CORE);
+
+        mockLicenseDetail = mock(LicenseDetails.class);
+        when(mockLicenseDetail.getLicenseString()).thenReturn(LICENSE);
+        when(mockLicenseDetail.getLicensedApplications()).thenReturn(licensedApplications);
+
+        applicationHelper = new JiraApplicationHelper(
+                new MockI18nBean.MockI18nBeanFactory(),
+                licenseManager,
+                licenseService);
+    }
+
+    @Test
+    public void testGetLicenses() {
+        when(licenseManager.getLicenses()).thenReturn(Collections.singleton(mockLicenseDetail));
+        final Collection<LicenseDetails> licenses = applicationHelper.getLicenses();
+        assertThat(licenses, containsInAnyOrder(mockLicenseDetail));
+    }
+
+    @Test
+    public void testSetLicense() {
+        final JiraLicenseService.ValidationResult mockValidationResult = mock(JiraLicenseService.ValidationResult.class);
+        when(mockValidationResult.getErrorCollection()).thenReturn(new SimpleErrorCollection());
+        when(mockValidationResult.getLicenseString()).thenReturn(LICENSE);
+
+        when(licenseService.validate(any(I18nHelper.class), anyString())).thenReturn(mockValidationResult);
+        when(licenseManager.setLicenseNoEvent(mockValidationResult.getLicenseString())).thenReturn(mockLicenseDetail);
+
+        final LicenseDetails licenseDetails = applicationHelper.setLicense("AAA...");
+        assertThat(licenseDetails, equalTo(mockLicenseDetail));
+    }
+
+}
