3cec0c9336b2787738db6f0df2dde9aec3700195
Patrick Hobusch <patrick.hobusch@gmail.com>
2020-04-21 18:14:09 +0800

    Fix build after changes in ConfAPI Commons

--- src/main/java/de/aservo/atlassian/jira/confapi/rest/DirectoriesResourceImpl.java
+++ src/main/java/de/aservo/atlassian/jira/confapi/rest/DirectoriesResourceImpl.java
@@ -3,7 +3,7 @@ package de.aservo.atlassian.jira.confapi.rest;
 import de.aservo.atlassian.confapi.constants.ConfAPI;
 import de.aservo.atlassian.confapi.model.DirectoryBean;
 import de.aservo.atlassian.confapi.model.ErrorCollection;
-import de.aservo.atlassian.confapi.rest.api.DirectoryResource;
+import de.aservo.atlassian.confapi.rest.api.DirectoriesResource;
 import de.aservo.atlassian.confapi.service.api.DirectoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +12,9 @@ import org.springframework.stereotype.Component;
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
@@ -20,15 +22,16 @@ import static com.google.common.base.Preconditions.checkNotNull;
 import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 
 @Path(ConfAPI.DIRECTORIES)
+@Produces(MediaType.APPLICATION_JSON)
 @Component
-public class DirectoryResourceImpl implements DirectoryResource {
+public class DirectoriesResourceImpl implements DirectoriesResource {
 
-    private static final Logger log = LoggerFactory.getLogger(DirectoryResourceImpl.class);
+    private static final Logger log = LoggerFactory.getLogger(DirectoriesResourceImpl.class);
 
     private final DirectoryService directoryService;
 
     @Inject
-    public DirectoryResourceImpl(DirectoryService directoryService) {
+    public DirectoriesResourceImpl(DirectoryService directoryService) {
         this.directoryService = checkNotNull(directoryService);
     }
 
